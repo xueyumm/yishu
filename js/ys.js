66806a4296fd1f3b09c6991c4ba019a7de54d4e1
Administrator <2250779607@qq.com>
2017-03-15 17:34:57 +0800

    Initial commit

---
+++ js/ys.js
@@ -0,0 +1,9 @@
+//middle_last的点击后变色
+/*$(document).ready(function(){
+  $("middle_last li").mousedown(function(){
+    $("middle_last li").css("color","red");
+  });
+  $("middle_last li").mouseleave(function(){
+    $("middle_last li").css("color","#333");
+  });
+});*/
